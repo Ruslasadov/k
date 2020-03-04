b5139a0e40691e59f61093c6e34b6ca63f26600e
Ruslasadov <58813459+Ruslasadov@users.noreply.github.com>
2020-03-04 14:53:38 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+$('.spoiler').hide();
+$('.headimg').slideDown(2000);
+$('.spoilerButton').on('click', function () {
+  $('.spoilerButton').hide();
+  $('.spoiler').slideDown(2000);
+});
+$('.spoile').hide();
+
+$('.spoileButton').on('click', function () {
+  $('.spoileButton').hide();
+  $('.spoile').slideDown(2000);
+});
+  
+
+
